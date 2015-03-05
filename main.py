d31bfc4a6237f09b5a09267ad63938eab09de616
haoyuxu <haoyuxu@Ctrip.com>
2015-03-05 18:25:22 +0800

    add svd

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Wed Mar 04 15:31:20 2015
 import feature_prep
 import predict
 import pandas as pd
+from numpy import linalg as la  
 
 PATH = '.\\..\\data\\'
 
@@ -20,13 +21,8 @@ user_item_matrix, item_item_matrix = feature_prep.Feature.main(df)
 
 df = predict.Predict.main(user_item_matrix, item_item_matrix, df)
 
-user_item_all = user_item_matrix.loc[2,:]
-## all the item that user has clicked
-user_clicked_item = list(user_item_all[user_item_all>0].index)
-user_item_rating = list(user_item_matrix.loc[userid,user_clicked_item])
-## get all items interact with itemid in item_item_matrix, and calculate prob
-all_related_item = list(item_item_matrix.loc[itemid,user_clicked_item])
+print sum(abs(df.rating-df.score))/len(df)
 
-## for each clicked vector get a score
-all_related_item_positive_distance = np.dot(user_item_rating,all_related_item)*1.0/sum(all_related_item)
-return all_related_item_positive_distance   
+##SVD 
+
+U,sigma,VT=la.svd(user_item_matrix) 
--- predict.py
+++ predict.py
@@ -13,16 +13,24 @@ class Predict:
     
     @staticmethod
     def calculate_item_clicked_probability(user_item_matrix, item_item_matrix, userid, itemid):
-        user_item_all = user_item_matrix.loc[2,:]
+        user_item_all = user_item_matrix.loc[userid,:]
         ## all the item that user has clicked
         user_clicked_item = list(user_item_all[user_item_all>0].index)
-        user_item_rating = list(user_item_matrix.loc[userid,user_clicked_item])
+        user_item_rating = user_item_matrix.loc[userid,user_clicked_item].values
         ## get all items interact with itemid in item_item_matrix, and calculate prob
-        all_related_item = list(item_item_matrix.loc[itemid,user_clicked_item])
+        all_related_item = item_item_matrix.loc[itemid,user_clicked_item].values
+        
+        related_item_vector = [(all_related_item > 0.35) & (all_related_item!=1)]
         
         ## for each clicked vector get a score
+        user_item_rating = user_item_rating[related_item_vector]
+        all_related_item = all_related_item[related_item_vector]
+        
+        #print user_item_rating
+        #rint all_related_item
+        
         all_related_item_positive_distance = np.dot(user_item_rating,all_related_item)*1.0/sum(all_related_item)
-        return all_related_item_positive_distance   
+        return all_related_item_positive_distance 
     
     
     @staticmethod
